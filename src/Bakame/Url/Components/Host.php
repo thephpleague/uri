b44dc2e92ed72d784cbb7ba36cbe897a3a636d6b
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2013-11-20 13:23:39 +0100

    Move components classes to their own directory

---
+++ src/Bakame/Url/Components/Host.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* Bakame.url - A lightweight Url Parser library
+*
+* @author Ignace Nyamagana Butera <nyamsprod@gmail.com>
+* @copyright 2013 Ignace Nyamagana Butera
+* @link https://github.com/nyamsprod/Bakame.url
+* @license http://opensource.org/licenses/MIT
+* @version 1.0.0
+* @package Bakame.url
+*
+* MIT LICENSE
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+namespace Bakame\Url\Components;
+
+use RuntimeException;
+
+/**
+ *  A Class to manipulate URL segment like component
+ *
+ * @package Bakame.Url
+ *
+ */
+class Host extends Segment
+{
+
+    public function __construct($str)
+    {
+        parent::__construct($str, '.');
+    }
+
+    /**
+     * Set Host values
+     * @param mixed   $value            a string OR an array representing the data to be inserted
+     * @param string  $position         append or prepend where to insert the data in the host array
+     * @param integer $valueBefore      the data where to append the $value
+     * @param integer $valueBeforeIndex the occurenceIndex of $valueBefore if $valueBefore appears more than once
+     *
+     * @throws RuntimeException If numbers of component exceeds or is equals to 127
+     * @throws RuntimeException If host length exceeds or is equals to 255
+     *
+     * @return self
+     */
+    public function set($value, $position = null, $valueBefore = null, $valueBeforeIndex = null)
+    {
+        if (! is_array($value)) {
+            $value = (array) $value;
+        }
+
+        if (127 <= (count($this->data) + count($value))) {
+            throw new RuntimeException('Host may have at maximum 127 parts');
+        }
+
+        if (strlen($this->__toString()) + strlen(implode($this->separator, $value)) + strlen($this->separator) > 255) {
+            throw new RuntimeException('Host may have a maximum of 255 characters');
+        }
+
+        if ('prepend' !== $position) {
+            $position = 'append';
+        }
+
+        return $this->$position($value, $valueBefore, $valueBeforeIndex);
+    }
+}
---
+++ src/Bakame/Url/Components/Port.php
@@ -0,0 +1,94 @@
+<?php
+/**
+* Bakame.url - A lightweight Url Parser library
+*
+* @author Ignace Nyamagana Butera <nyamsprod@gmail.com>
+* @copyright 2013 Ignace Nyamagana Butera
+* @link https://github.com/nyamsprod/Bakame.url
+* @license http://opensource.org/licenses/MIT
+* @version 1.0.0
+* @package Bakame.url
+*
+* MIT LICENSE
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+namespace Bakame\Url\Components;
+
+/**
+ *  A Class to manipulate URL port component
+ *
+ * @package Bakame.Url
+ *
+ */
+class Port
+{
+    /**
+     * Port content
+     * @var string
+     */
+    private $data;
+
+    public function __construct($str = null)
+    {
+        $this->set($str);
+    }
+
+    /**
+     * get the Port value
+     * @return string
+     */
+    public function get()
+    {
+        return $this->data;
+    }
+
+    /**
+     * set the Port value
+     * @param string $value
+     *
+     * @return self
+     */
+    public function set($value = null)
+    {
+        if (null !== $value) {
+            $value = filter_var($value, FILTER_VALIDATE_INT, array(
+                'options' => array('min_range' => 1, 'default' => 80)
+            ));
+        }
+        $this->data = $value;
+
+        return $this;
+    }
+
+    /**
+     * format the string representation
+     * @return string
+     */
+    public function __toString()
+    {
+        $str = '';
+        if (! empty($this->data)) {
+            $str = ':'.$this->data;
+        }
+
+        return $str;
+    }
+}

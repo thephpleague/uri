ea78513ff246258194d42ec39a5570f934802560
ignace nyamagana butera <nyamsprod@gmail.com>
2014-05-24 14:39:24 +0200

    Code improved removed duplicate code
    adding validateComponent method
    refactoring removeSegment method

--- src/Validation.php
+++ src/Validation.php
@@ -57,24 +57,6 @@ abstract class Validation
     }
 
     /**
-     * Sanitize a string component
-     *
-     * @param string $str
-     *
-     * @return string|null
-     */
-    protected static function sanitizeComponent($str)
-    {
-        if (is_null($str)) {
-            return $str;
-        }
-        $str = filter_var((string) $str, FILTER_UNSAFE_RAW, array('flags' => FILTER_FLAG_STRIP_LOW));
-        $str = trim($str);
-
-        return $str;
-    }
-
-    /**
      * Validate the URL Port component
      *
      * @param integer $str
@@ -121,66 +103,31 @@ abstract class Validation
     /**
      * Validate data before insertion into a URL segment based component
      *
-     * @param mixed  $data      the data to insert
-     * @param string $delimiter a single character delimiter
+     * @param mixed  $data     the data to insert
+     * @param string $callback a callable function to be called to parse
+     *                         a given string into the corrseponding component
      *
      * @return array
      *
      * @throws RuntimeException if the data is not valid
      */
-    protected static function validateSegment($data, $delimiter)
+    protected static function validateComponent($data, $callback)
     {
         if (is_null($data)) {
             return array();
         } elseif ($data instanceof Traversable) {
-            return iterator_to_array($data, false); //we don't need the index
+            return iterator_to_array($data);
         } elseif (is_string($data) || (is_object($data)) && (method_exists($data, '__toString'))) {
             $data = (string) $data;
             $data = trim($data);
-            if (isset($data[0]) && $delimiter == $data[0]) {
-                $data = substr($data, 1);
+            if ('' == $data) {
+                return array();
             }
-
-            return explode($delimiter, $data);
+            $data = $callback($data);
         }
 
         if (! is_array($data)) {
             throw new RuntimeException('Your submitted data could not be converted into a proper array');
-        }
-
-        return $data;
-    }
-
-
-    /**
-     * Validate Host data before insertion into a URL host component
-     *
-     * @param mixed $data the data to insert
-     * @param array $host a annray representation of a host component
-     *
-     * @return array
-     *
-     * @throws InvalidArgumentException If the added is invalid
-     */
-    protected static function validateHost($data, array $host = array())
-    {
-        if (is_null($data)) {
-            return array();
-        }
-
-        $data = self::validateSegment($data, '.');
-        $imploded = implode('.', $data);
-        if (127 <= (count($host) + count($data))) {
-            throw new InvalidArgumentException('Host may have at maximum 127 parts');
-        } elseif (225 <= (strlen(implode('.', $host)) + strlen($imploded) + 1)) {
-            throw new InvalidArgumentException('Host may have a maximum of 255 characters');
-        } elseif (strpos($imploded, ' ') !== false || strpos($imploded, '_') !== false) {
-            throw new InvalidArgumentException('Invalid Characters used to create your host');
-        }
-        foreach ($data as $value) {
-            if (strlen($value) > 63) {
-                throw new InvalidArgumentException('each label host must have a maximum of 63 characters');
-            }
         }
 
         return $data;
@@ -197,26 +144,63 @@ abstract class Validation
      */
     protected static function validateQuery($data)
     {
-        if (is_null($data)) {
-            return array();
-        } elseif ($data instanceof Traversable) {
-            return iterator_to_array($data);
-        } elseif (is_string($data) || (is_object($data)) && (method_exists($data, '__toString'))) {
-            $data = (string) $data;
-            $data = trim($data);
-            if ('' == $data) {
-                return array();
+        return self::validateComponent($data, function ($str) {
+            if ('?' == $str[0]) {
+                $str = substr($str, 1);
             }
-            if ('?' == $data[0]) {
-                $data = substr($data, 1);
-            }
-            parse_str($data, $res);
+            parse_str($str, $arr);
 
-            return $res;
+            return $arr;
+        });
+    }
+
+    /**
+     * Validate data before insertion into a URL segment based component
+     *
+     * @param mixed  $data      the data to insert
+     * @param string $delimiter a single character delimiter
+     *
+     * @return array
+     *
+     * @throws RuntimeException if the data is not valid
+     */
+    protected static function validateSegment($data, $delimiter)
+    {
+        return self::validateComponent($data, function ($str) use ($delimiter) {
+            if ($delimiter == $str[0]) {
+                $str = substr($str, 1);
+            }
+
+            return explode($delimiter, $str);
+        });
+    }
+
+
+    /**
+     * Validate Host data before insertion into a URL host component
+     *
+     * @param mixed $data the data to insert
+     * @param array $host an array representation of a host component
+     *
+     * @return array
+     *
+     * @throws InvalidArgumentException If the added is invalid
+     */
+    protected static function validateHost($data, array $host = array())
+    {
+        $data = self::validateSegment($data, '.');
+        $imploded = implode('.', $data);
+        if (127 <= (count($host) + count($data))) {
+            throw new InvalidArgumentException('Host may have at maximum 127 parts');
+        } elseif (225 <= (strlen(implode('.', $host)) + strlen($imploded) + 1)) {
+            throw new InvalidArgumentException('Host may have a maximum of 255 characters');
+        } elseif (strpos($imploded, ' ') !== false || strpos($imploded, '_') !== false) {
+            throw new InvalidArgumentException('Invalid Characters used to create your host');
         }
-
-        if (! is_array($data)) {
-            throw new RuntimeException('Your submitted data could not be converted into a proper array');
+        foreach ($data as $value) {
+            if (strlen($value) > 63) {
+                throw new InvalidArgumentException('each label host must have a maximum of 63 characters');
+            }
         }
 
         return $data;
@@ -291,15 +275,32 @@ abstract class Validation
             return $data;
         }
 
-        $data = array();
-        $res = explode($delimiter, substr($segment, 0, $pos).substr($segment, $pos + strlen($part)));
-        foreach ($res as $value) {
-            if (! is_null($value) && '' != $value) {
-                $data[] = $value;
-            }
+        $raw = substr($segment, 0, $pos).substr($segment, $pos + strlen($part));
+        if ('.' == $delimiter) {
+            $data = self::validateHost($raw);
+        } elseif ('/' == $delimiter) {
+            $data = self::validateSegment($raw, $delimiter);
         }
 
         return $data;
+    }
+
+    /**
+     * Sanitize a string component
+     *
+     * @param string $str
+     *
+     * @return string|null
+     */
+    protected static function sanitizeComponent($str)
+    {
+        if (is_null($str)) {
+            return $str;
+        }
+        $str = filter_var((string) $str, FILTER_UNSAFE_RAW, array('flags' => FILTER_FLAG_STRIP_LOW));
+        $str = trim($str);
+
+        return $str;
     }
 
     /**
@@ -309,7 +310,7 @@ abstract class Validation
      *
      * @return array
      */
-    protected function sanitizeComponents(array $components)
+    protected static function sanitizeComponents(array $components)
     {
         $components = array_merge(array(
             'scheme' => null,

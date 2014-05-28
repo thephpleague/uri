e1f35993c9283a6df8ca13ad4277e2dc734b8663
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-05-28 11:40:13 +0200

    Docblocks update

--- test/UrlTest.php
+++ test/UrlTest.php
@@ -121,66 +121,66 @@ class UrlTest extends PHPUnit_Framework_TestCase
      */
     public function testQuery()
     {
-        $this->assertSame('kingkong=toto&john=doe+the+john', $this->url->modifyQuery(array('john' => 'doe the john'))->getQuery());
-        $this->assertSame('kingkong=toto&john=doe+the+john', $this->url->modifyQuery(new ArrayIterator(array('john' => 'doe the john')))->getQuery());
-        $this->assertSame('kingkong=tata', $this->url->modifyQuery('?kingkong=tata')->getQuery());
-        $this->assertSame('kingkong=toto', $this->url->modifyQuery('')->getQuery());
-        $this->assertNull($this->url->setQuery(null)->getQuery());
-        $this->assertSame('ali=baba', $this->url->setQuery(array('ali' => 'baba'))->getQuery());
-        $this->assertSame('kingkong=toto', $this->url->getQuery());
+        $this->assertSame('kingkong=toto&john=doe+the+john', (string) $this->url->modifyQuery(array('john' => 'doe the john'))->getQuery());
+        $this->assertSame('kingkong=toto&john=doe+the+john', (string) $this->url->modifyQuery(new ArrayIterator(array('john' => 'doe the john')))->getQuery());
+        $this->assertSame('kingkong=tata', (string) $this->url->modifyQuery('?kingkong=tata')->getQuery());
+        $this->assertSame('kingkong=toto', (string) $this->url->modifyQuery('')->getQuery());
+        $this->assertNull($this->url->setQuery(null)->getQuery()->get());
+        $this->assertSame('ali=baba', (string) $this->url->setQuery(array('ali' => 'baba'))->getQuery());
+        $this->assertSame('kingkong=toto', (string) $this->url->getQuery());
         $this->url->modifyQuery(new StdClass);
     }
 
     public function testPath()
     {
-        $this->assertSame('master/test/query.php', $this->url->prependPath('master')->getPath());
-        $this->assertSame('query.php', $this->url->removePath('test')->getPath());
-        $this->assertSame('test/query.php', $this->url->removePath('toto')->getPath());
-        $this->assertSame('test/sullivent/query.php', $this->url->appendPath('sullivent', 'test')->getPath());
-        $this->assertSame('shop/checkout', $this->url->setPath('/shop/checkout')->getPath());
-        $this->assertSame('shop/rev%20iew', $this->url->setPath(array('shop', 'rev iew'))->getPath());
-        $this->assertNull($this->url->setPath(null)->getPath());
-        $this->assertSame('test/query.php', $this->url->getPath());
+        $this->assertSame('master/test/query.php', (string) $this->url->prependPath('master')->getPath());
+        $this->assertSame('query.php', (string) $this->url->removePath('test')->getPath());
+        $this->assertSame('test/query.php', (string) $this->url->removePath('toto')->getPath());
+        $this->assertSame('test/sullivent/query.php', (string) $this->url->appendPath('sullivent', 'test')->getPath());
+        $this->assertSame('shop/checkout', (string) $this->url->setPath('/shop/checkout')->getPath());
+        $this->assertSame('shop/rev%20iew', (string) $this->url->setPath(array('shop', 'rev iew'))->getPath());
+        $this->assertNull($this->url->setPath(null)->getPath()->get());
+        $this->assertSame('test/query.php', (string) $this->url->getPath());
 
-        $this->assertSame('test/sullivent/wacowski/query.php', $this->url->appendPath(new ArrayIterator(array('sullivent', 'wacowski')), 'test')->getPath());
+        $this->assertSame('test/sullivent/wacowski/query.php', (string) $this->url->appendPath(new ArrayIterator(array('sullivent', 'wacowski')), 'test')->getPath());
 
         $url = $this->url
             ->prependPath('master')
             ->prependPath('master');
 
-        $this->assertSame('master/slave/master/test/query.php', $url->appendPath('slave', 'master', 0)->getPath());
+        $this->assertSame('master/slave/master/test/query.php', (string) $url->appendPath('slave', 'master', 0)->getPath());
 
         $url = $this->url
             ->appendPath('master', 'test')
             ->appendPath('master', 'test');
 
-        $this->assertSame('test/slave/master/master/query.php', $url->prependPath('slave', 'master', 0)->getPath());
+        $this->assertSame('test/slave/master/master/query.php', (string) $url->prependPath('slave', 'master', 0)->getPath());
     }
 
     public function testHost()
     {
-        $this->assertSame('master.secure.example.com', $this->url->prependHost('master')->getHost());
-        $this->assertSame('example.com', $this->url->removeHost('secure')->getHost());
-        $this->assertSame('secure.example.com', $this->url->removeHost('toto')->getHost());
-        $this->assertSame('secure.shop.example.com', $this->url->appendHost('shop', 'secure')->getHost());
-        $this->assertSame('shop.fremium.com', $this->url->setHost('.shop.fremium.com')->getHost());
-        $this->assertSame('shop.premium.org', $this->url->setHost(array('shop', 'premium', 'org'))->getHost());
-        $this->assertSame('shop.premium.org', $this->url->setHost(new ArrayIterator(array('shop', 'premium', 'org')))->getHost());
-        $this->assertNull($this->url->setHost(null)->getHost());
-        $this->assertSame('secure.example.com', $this->url->getHost());
+        $this->assertSame('master.secure.example.com', $this->url->prependHost('master')->getHost()->get());
+        $this->assertSame('example.com', $this->url->removeHost('secure')->getHost()->get());
+        $this->assertSame('secure.example.com', $this->url->removeHost('toto')->getHost()->get());
+        $this->assertSame('secure.shop.example.com', $this->url->appendHost('shop', 'secure')->getHost()->get());
+        $this->assertSame('shop.fremium.com', $this->url->setHost('.shop.fremium.com')->getHost()->get());
+        $this->assertSame('shop.premium.org', $this->url->setHost(array('shop', 'premium', 'org'))->getHost()->get());
+        $this->assertSame('shop.premium.org', $this->url->setHost(new ArrayIterator(array('shop', 'premium', 'org')))->getHost()->get());
+        $this->assertNull($this->url->setHost(null)->getHost()->get());
+        $this->assertSame('secure.example.com', $this->url->getHost()->get());
     }
 
     public function testParse()
     {
         $expected = array (
-            'scheme' => $this->url->getScheme(),
-            'user' => $this->url->getUser(),
-            'pass' => $this->url->getPass(),
-            'host' => $this->url->getHost(),
-            'port' => $this->url->getPort(),
-            'path' => $this->url->getPath(),
-            'query' => $this->url->getQuery(),
-            'fragment' => $this->url->getFragment(),
+            'scheme' => $this->url->getScheme()->get(),
+            'user' => $this->url->getUser()->get(),
+            'pass' => $this->url->getPass()->get(),
+            'host' => $this->url->getHost()->get(),
+            'port' => $this->url->getPort()->get(),
+            'path' => $this->url->getPath()->get(),
+            'query' => $this->url->getQuery()->get(),
+            'fragment' => $this->url->getFragment()->get(),
         );
         $this->assertSame($expected, $this->url->parse());
         $this->assertSame(Query::PHP_QUERY_RFC1738, $this->url->getEncodingType());
@@ -193,9 +193,9 @@ class UrlTest extends PHPUnit_Framework_TestCase
         $this->assertSame('https://sullivent:wacowski@secure.example.com:443/test/query.php?kingkong=toto#doc3', (string) $this->url->setUser('sullivent')->setPass('wacowski'));
         $this->assertSame('http://login:pass@secure.example.com/test/query.php?kingkong=toto#doc3', (string) $this->url->setScheme('http')->setPort(null));
         $this->assertSame('https://login:pass@secure.example.com:443/test/query.php?kingkong=toto#payment', (string) $this->url->setFragment('payment'));
-        $this->assertSame('login', $this->url->getUser());
-        $this->assertSame('pass', $this->url->getPass());
-        $this->assertSame(443, $this->url->getPort());
-        $this->assertSame('doc3', $this->url->getFragment());
+        $this->assertSame('login', $this->url->getUser()->get());
+        $this->assertSame('pass', $this->url->getPass()->get());
+        $this->assertSame(443, $this->url->getPort()->get());
+        $this->assertSame('doc3', $this->url->getFragment()->get());
     }
 }

fb9e2ecda66e91992334f8b433d1a4ac467bab71
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-06-11 09:38:13 +0200

    remove AbstractUrl class

--- src/Url.php
+++ src/Url.php
@@ -12,101 +12,164 @@
 */
 namespace League\Url;
 
+use League\Url\Interfaces\EncodingInterface;
+use League\Url\Interfaces\UrlInterface;
+use League\Url\Interfaces\QueryInterface;
+use League\Url\Interfaces\SegmentInterface;
+use League\Url\Interfaces\ComponentInterface;
+
 /**
  * A class to manipulate URLs
  *
  * @package League.url
  */
-class Url extends AbstractUrl
+class Url implements EncodingInterface, UrlInterface
 {
     /**
-     * {@inheritdoc}
-     */
-    public function setUser($data)
-    {
-        $this->user->set($data);
+    * Scheme
+    *
+    * @var {@link ComponentInterface}  Object
+    */
+    protected $scheme;
 
-        return $this;
+    /**
+    * User
+    *
+    * @var {@link ComponentInterface} Object
+    */
+    protected $user;
+
+    /**
+    * Pass
+    *
+    * @var {@link ComponentInterface} Object
+    */
+    protected $pass;
+
+    /**
+     * Host
+     *
+     * @var {@link SegmentInterface} Object
+     */
+    protected $host;
+
+    /**
+     * Port
+     *
+     *@var {@link ComponentInterface} Object
+     */
+    protected $port;
+
+    /**
+     * Path
+     *
+     * @var {@link SegmentInterface} Object
+     */
+    protected $path;
+
+    /**
+     * Query
+     *
+     * @var {@link QueryInterface} Object
+     */
+    protected $query;
+
+    /**
+     * Fragment
+     *
+     * @var {@link ComponentInterface} Object
+     */
+    protected $fragment;
+
+    /**
+     * The Constructor
+     *
+     * @param {@link ComponentInterface} $scheme   Url Scheme Component object
+     * @param {@link ComponentInterface} $user     Url User Component object
+     * @param {@link ComponentInterface} $pass     Url Pass Component object
+     * @param {@link SegmentInterface}   $host     Url Host Component object
+     * @param {@link ComponentInterface} $port     Url Port Component object
+     * @param {@link SegmentInterface}   $path     Url Path Component object
+     * @param {@link QueryInterface}     $query    Url Query Component object
+     * @param {@link ComponentInterface} $fragment Url Fragment Component object
+     */
+    public function __construct(
+        ComponentInterface $scheme,
+        ComponentInterface $user,
+        ComponentInterface $pass,
+        SegmentInterface $host,
+        ComponentInterface $port,
+        SegmentInterface $path,
+        QueryInterface $query,
+        ComponentInterface $fragment
+    ) {
+        $this->scheme = $scheme;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->host = $host;
+        $this->port = $port;
+        $this->path = $path;
+        $this->query = $query;
+        $this->fragment = $fragment;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getUser()
+    public function __toString()
     {
-        return $this->user;
+        return $this->getBaseUrl().$this->getRelativeUrl();
     }
 
     /**
      * {@inheritdoc}
      */
-    public function setPass($data)
+    public function getRelativeUrl()
     {
-        $this->pass->set($data);
+        $path = $this->path->getUriComponent();
+        $query = $this->query->getUriComponent();
+        $fragment = $this->fragment->getUriComponent();
+        if ('' == $path) {
+            $path = '/'.$path;
+        }
 
-        return $this;
+        return $path.$query.$fragment;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getPass()
+    public function getBaseUrl()
     {
-        return $this->pass;
+        $scheme = $this->scheme->getUriComponent();
+        $user = $this->user->getUriComponent();
+        $pass = $this->pass->getUriComponent();
+        $host = $this->host->getUriComponent();
+        $port = $this->port->getUriComponent();
+
+        $user .= $pass;
+        if ('' != $user) {
+            $user .= '@';
+        }
+
+        if ('' != $host && '' == $scheme) {
+            $scheme = '//';
+        }
+
+        return $scheme.$user.$host.$port;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function setPort($data)
+    public function sameValueAs(UrlInterface $url)
     {
-        $this->port->set($data);
+        $this_url = clone $this;
+        $that_url = clone $url;
+        $this_url = $this_url->setEncoding(PHP_QUERY_RFC1738);
+        $that_url = $that_url->setEncoding(PHP_QUERY_RFC1738);
 
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPort()
-    {
-        return $this->port;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setScheme($data)
-    {
-        $this->scheme->set($data);
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getScheme()
-    {
-        return $this->scheme;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setFragment($data)
-    {
-        $this->fragment->set($data);
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFragment()
-    {
-        return $this->fragment;
+        return  $this_url->__toString() == $that_url->__toString();
     }
 
     /**
@@ -122,6 +185,138 @@ class Url extends AbstractUrl
     /**
      * {@inheritdoc}
      */
+    public function getEncoding()
+    {
+        return $this->query->getEncoding();
+    }
+
+    /**
+     * Set the URL user component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
+    public function setUser($data)
+    {
+        $this->user->set($data);
+
+        return $this;
+    }
+
+    /**
+     * get the URL user component
+     *
+     * @return {@link ComponentInterface}
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the URL pass component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
+    public function setPass($data)
+    {
+        $this->pass->set($data);
+
+        return $this;
+    }
+
+    /**
+     * get the URL pass component
+     *
+     * @return {@link ComponentInterface}
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * Set the URL port component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
+    public function setPort($data)
+    {
+        $this->port->set($data);
+
+        return $this;
+    }
+
+    /**
+     * get the URL port component
+     *
+     * @return {@link ComponentInterface}
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set the URL scheme component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
+    public function setScheme($data)
+    {
+        $this->scheme->set($data);
+
+        return $this;
+    }
+
+    /**
+     * get the URL scheme component
+     *
+     * @return {@link ComponentInterface}
+     */
+    public function getScheme()
+    {
+        return $this->scheme;
+    }
+
+    /**
+     * Set the URL fragment component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
+    public function setFragment($data)
+    {
+        $this->fragment->set($data);
+
+        return $this;
+    }
+
+    /**
+     * get the URL fragment component
+     *
+     * @return {@link ComponentInterface}
+     */
+    public function getFragment()
+    {
+        return $this->fragment;
+    }
+
+    /**
+     * Set the URL query component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
     public function setQuery($data)
     {
         $this->query->set($data);
@@ -130,7 +325,9 @@ class Url extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL user component
+     *
+     * @return {@link QueryInterface}
      */
     public function getQuery()
     {
@@ -138,7 +335,11 @@ class Url extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL host component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setHost($data)
     {
@@ -148,7 +349,9 @@ class Url extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL host component
+     *
+     * @return {@link SegmentInterface}
      */
     public function getHost()
     {
@@ -156,7 +359,11 @@ class Url extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL path component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setPath($data)
     {
@@ -166,7 +373,9 @@ class Url extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL path component
+     *
+     * @return {@link SegmentInterface}
      */
     public function getPath()
     {
--- src/UrlImmutable.php
+++ src/UrlImmutable.php
@@ -12,13 +12,166 @@
 */
 namespace League\Url;
 
+use League\Url\Interfaces\EncodingInterface;
+use League\Url\Interfaces\UrlInterface;
+use League\Url\Interfaces\QueryInterface;
+use League\Url\Interfaces\SegmentInterface;
+use League\Url\Interfaces\ComponentInterface;
+
 /**
  * A Immutable Value Object class to manipulate URLs
  *
  * @package League.url
  */
-final class UrlImmutable extends AbstractUrl
+final class UrlImmutable implements EncodingInterface, UrlInterface
 {
+    /**
+    * Scheme
+    *
+    * @var {@link ComponentInterface}  Object
+    */
+    private $scheme;
+
+    /**
+    * User
+    *
+    * @var {@link ComponentInterface} Object
+    */
+    private $user;
+
+    /**
+    * Pass
+    *
+    * @var {@link ComponentInterface} Object
+    */
+    private $pass;
+
+    /**
+     * Host
+     *
+     * @var {@link SegmentInterface} Object
+     */
+    private $host;
+
+    /**
+     * Port
+     *
+     *@var {@link ComponentInterface} Object
+     */
+    private $port;
+
+    /**
+     * Path
+     *
+     * @var {@link SegmentInterface} Object
+     */
+    private $path;
+
+    /**
+     * Query
+     *
+     * @var {@link QueryInterface} Object
+     */
+    private $query;
+
+    /**
+     * Fragment
+     *
+     * @var {@link ComponentInterface} Object
+     */
+    private $fragment;
+
+    /**
+     * The Constructor
+     *
+     * @param {@link ComponentInterface} $scheme   Url Scheme object
+     * @param {@link ComponentInterface} $user     Url Component object
+     * @param {@link ComponentInterface} $pass     Url Component object
+     * @param {@link SegmentInterface}   $host     Url Host object
+     * @param {@link ComponentInterface} $port     Url Port object
+     * @param {@link SegmentInterface}   $path     Url Path object
+     * @param {@link QueryInterface}     $query    Url Query object
+     * @param {@link ComponentInterface} $fragment Url Component object
+     */
+    public function __construct(
+        ComponentInterface $scheme,
+        ComponentInterface $user,
+        ComponentInterface $pass,
+        SegmentInterface $host,
+        ComponentInterface $port,
+        SegmentInterface $path,
+        QueryInterface $query,
+        ComponentInterface $fragment
+    ) {
+        $this->scheme = $scheme;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->host = $host;
+        $this->port = $port;
+        $this->path = $path;
+        $this->query = $query;
+        $this->fragment = $fragment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return $this->getBaseUrl().$this->getRelativeUrl();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRelativeUrl()
+    {
+        $path = $this->path->getUriComponent();
+        $query = $this->query->getUriComponent();
+        $fragment = $this->fragment->getUriComponent();
+        if ('' == $path) {
+            $path = '/'.$path;
+        }
+
+        return $path.$query.$fragment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseUrl()
+    {
+        $scheme = $this->scheme->getUriComponent();
+        $user = $this->user->getUriComponent();
+        $pass = $this->pass->getUriComponent();
+        $host = $this->host->getUriComponent();
+        $port = $this->port->getUriComponent();
+
+        $user .= $pass;
+        if ('' != $user) {
+            $user .= '@';
+        }
+
+        if ('' != $host && '' == $scheme) {
+            $scheme = '//';
+        }
+
+        return $scheme.$user.$host.$port;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sameValueAs(UrlInterface $url)
+    {
+        $this_url = clone $this;
+        $that_url = clone $url;
+        $this_url = $this_url->setEncoding(PHP_QUERY_RFC1738);
+        $that_url = $that_url->setEncoding(PHP_QUERY_RFC1738);
+
+        return  $this_url->__toString() == $that_url->__toString();
+    }
+
     /**
      * To Enable cloning
      */
@@ -48,6 +201,18 @@ final class UrlImmutable extends AbstractUrl
     /**
      * {@inheritdoc}
      */
+    public function getEncoding()
+    {
+        return $this->query->getEncoding();
+    }
+
+    /**
+     * Set the URL user component
+     *
+     * @param string $data
+     *
+     * @return self
+     */
     public function setUser($data)
     {
         $clone = clone $this;
@@ -57,7 +222,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link ComponentInterface}
      */
     public function getUser()
     {
@@ -65,7 +232,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL pass component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setPass($data)
     {
@@ -76,7 +247,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link ComponentInterface}
      */
     public function getPass()
     {
@@ -84,7 +257,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL port component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setPort($value)
     {
@@ -95,7 +272,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link ComponentInterface}
      */
     public function getPort()
     {
@@ -103,7 +282,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL scheme component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setScheme($value)
     {
@@ -122,7 +305,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL fragment component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setFragment($data)
     {
@@ -133,7 +320,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link ComponentInterface}
      */
     public function getFragment()
     {
@@ -141,7 +330,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL query component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setQuery($data)
     {
@@ -152,7 +345,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link QueryInterface}
      */
     public function getQuery()
     {
@@ -160,7 +355,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL host component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setHost($data)
     {
@@ -171,7 +370,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link SegmentInterface}
      */
     public function getHost()
     {
@@ -179,7 +380,11 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * Set the URL path component
+     *
+     * @param string $data
+     *
+     * @return self
      */
     public function setPath($data)
     {
@@ -190,7 +395,9 @@ final class UrlImmutable extends AbstractUrl
     }
 
     /**
-     * {@inheritdoc}
+     * get the URL pass component
+     *
+     * @return {@link SegmentInterface}
      */
     public function getPath()
     {

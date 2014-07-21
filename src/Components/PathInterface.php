b6df0df7e1a8106cbbb999cf459257059f824b7c
ignace nyamagana butera <nyamsprod@gmail.com>
2014-07-21 21:32:05 +0200

    adding PathInterface

---
+++ src/Components/PathInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* This file is part of the League.url library
+*
+* @license http://opensource.org/licenses/MIT
+* @link https://github.com/thephpleague/url/
+* @version 3.2.0
+* @package League.url
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace League\Url\Components;
+
+/**
+ * A common interface for URL segment like component
+ *
+ *  @package League.url
+ *  @since  3.2.0
+ */
+interface PathInterface extends SegmentInterface
+{
+    /**
+     * return the string representation for a relative path
+     *
+     * @param integer $start_index the index to start from
+     *                             when adding relative segment path
+     *                             setting the relative URL
+     *
+     * @return string
+     */
+    public function getRelativePath($start_index = 0);
+}

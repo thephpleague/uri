2fd6cc0811e6d5231e63472669c8b90807f1daff
Jan Hohner <me@janhohner.de>
2014-09-01 09:35:29 +0200

    Added support for dots in the query string keys

--- src/Components/Query.php
+++ src/Components/Query.php
@@ -114,7 +114,13 @@ class Query extends AbstractArray implements QueryInterface, ArrayAccess
             if ('?' == $str[0]) {
                 $str = substr($str, 1);
             }
+            
+            // from: https://stackoverflow.com/a/22539704
+            $str = preg_replace_callback('/(?:^|(?<=&))[^=[]+/', function($match) {
+                return bin2hex(urldecode($match[0]));
+            }, $str);
             parse_str($str, $arr);
+            $arr = array_combine(array_map('hex2bin', array_keys($arr)), $arr);
 
             return $arr;
         });

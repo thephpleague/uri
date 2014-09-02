0152de2f121946a191b49acd51841f1dce908891
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-09-02 09:51:24 +0200

    bug fix pull request #32

--- src/Components/Query.php
+++ src/Components/Query.php
@@ -114,13 +114,18 @@ class Query extends AbstractArray implements QueryInterface, ArrayAccess
             if ('?' == $str[0]) {
                 $str = substr($str, 1);
             }
-            
-            // from: https://stackoverflow.com/a/22539704
-            $str = preg_replace_callback('/(?:^|(?<=&))[^=[]+/', function($match) {
+
+            //let's preserve the key params
+            $str = preg_replace_callback('/(?:^|(?<=&))[^=[]+/', function ($match) {
                 return bin2hex(urldecode($match[0]));
             }, $str);
             parse_str($str, $arr);
-            $arr = array_combine(array_map('hex2bin', array_keys($arr)), $arr);
+
+            //hexbin does not work in PHP 5.3
+            $arr = array_combine(array_map(function ($value) {
+                return pack('H*', $value);
+
+            }, array_keys($arr)), $arr);
 
             return $arr;
         });

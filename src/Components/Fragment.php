28340526b937b888b73e4096d568eb8a4388696d
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2014-11-07 11:33:27 +0000

    [fragment] Do not escape allowed symbols

--- src/Components/Fragment.php
+++ src/Components/Fragment.php
@@ -27,7 +27,14 @@ class Fragment extends AbstractComponent
     {
         $value = parent::__toString();
 
-        return str_replace(array('%2F', '%3F'), array('/', '?'), rawurlencode($value));
+        // according to http://tools.ietf.org/html/rfc3986#section-3.5
+        $allowedSymbols = array('/', '?', '-', '.', '_', '~', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=', ':', '@');
+
+        $encodedAllowedSymbols = array_map(function($symbol) {
+            return urlencode($symbol);
+        }, $allowedSymbols);
+
+        return str_replace($encodedAllowedSymbols, $allowedSymbols, rawurlencode($value));
     }
 
     /**

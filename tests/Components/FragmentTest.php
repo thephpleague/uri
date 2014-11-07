b632d6cf1d66cc25689aaf2ef502c09f4cf70e18
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2014-11-07 11:33:27 +0000

    [fragment] Do not escape allowed symbols

---
+++ tests/Components/FragmentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace League\Url\Test\Components;
+
+use ArrayIterator;
+use League\Url\Components\Fragment;
+use League\Url\Components\Query;
+use PHPUnit_Framework_TestCase;
+use StdClass;
+
+/**
+ * @group components
+ */
+class FragmentTest extends PHPUnit_Framework_TestCase
+{
+    public function testAllowedSymbolsNotEncodedWhenCastToString()
+    {
+        $fragment = new Fragment("azAZ0-9/?-._~!$&'()*+,;=:@");
+
+        $this->assertEquals("azAZ0-9/?-._~!$&'()*+,;=:@", (string) $fragment);
+    }
+
+    public function testNotAllowedSymbolsEncodedWhenCastToString()
+    {
+        $fragment = new Fragment("#%^[]{}\"<>\\");
+
+        $this->assertEquals("%23%25%5E%5B%5D%7B%7D%22%3C%3E%5C", (string) $fragment);
+    }
+}

279b020cb6d8c68a930c6cdd4388ddc2d60dac7d
ignace nyamagana butera <nyamsprod@gmail.com>
2015-03-29 20:52:50 +0200

    Create StringValidator.php

---
+++ src/Util/StringValidator.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the League.url library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/thephpleague/url/
+ * @version 4.0.0
+ * @package League.url
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace League\Url\Util;
+
+use InvalidArgumentException;
+
+/**
+ * A trait to validate stringable data
+ *
+ * @package League.url
+ * @since 1.0.0
+ */
+trait StringValidator
+{
+    function validateString($str)
+    {
+        if (! is_scalar($str) && (is_object($str) && ! method_exists($str, '__toString'))) {
+            throw new InvalidArgumentException(sprintf(
+                'Data passed to the method must be a string; received "%s"',
+                (is_object($str) ? get_class($str) : gettype($str))
+            ));
+        }
+
+        return trim($str);
+    }   
+}

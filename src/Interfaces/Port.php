f8b9150070cd8e91df7907fbd8167cfe3c050f13
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2015-05-15 09:13:17 +0200

    implements Port::toInt method

---
+++ src/Interfaces/Port.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* This file is part of the League.url library
+*
+* @license http://opensource.org/licenses/MIT
+* @link https://github.com/thephpleague/url/
+* @version 4.0.0
+* @package League.url
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace League\Url\Interfaces;
+
+/**
+ * Value object representing a URL Port component.
+ *
+ * Instances of this interface are considered immutable; all methods that
+ * might change state MUST be implemented such that they retain the internal
+ * state of the current instance and return an instance that contains the
+ * changed state.
+ *
+ * @package  League.url
+ * @since  4.0.0
+ */
+interface Port extends Component
+{
+    /**
+     * Return an integer representation of the Port component
+     *
+     * @return null|int
+     */
+    public function toInt();
+}

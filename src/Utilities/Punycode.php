2d3ce5705e8fef7a86ebc47fa04957824b2bd6c2
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2015-06-09 13:51:07 +0200

    Improve Punycode code

--- src/Utilities/Punycode.php
+++ src/Utilities/Punycode.php
@@ -30,28 +30,28 @@ trait Punycode
      *
      * @param array
      */
-    protected static $encodeTable = [];
+    protected static $encodeTable = [
+        0  => 'a', 1  => 'b', 2  => 'c', 3  => 'd', 4  => 'e', 5  => 'f',
+        6  => 'g', 7  => 'h', 8  => 'i', 9  => 'j', 10 => 'k', 11 => 'l',
+        12 => 'm', 13 => 'n', 14 => 'o', 15 => 'p', 16 => 'q', 17 => 'r',
+        18 => 's', 19 => 't', 20 => 'u', 21 => 'v', 22 => 'w', 23 => 'x',
+        24 => 'y', 25 => 'z', 26 =>   0, 27 =>   1, 28 =>   2, 29 =>   3,
+        30 =>   4, 31 =>   5, 32 =>   6, 33 =>   7, 34 =>   8, 35 =>   9,
+    ];
 
     /**
      * Decode table
      *
      * @param array
      */
-    protected static $decodeTable = [];
-
-    /**
-     * Initialize encoding/decoding Table
-     */
-    protected static function initTable()
-    {
-        if (empty(static::$encodeTable)) {
-            static::$encodeTable = array_merge(range('a', 'z'), range(0, 9));
-        }
-
-        if (empty(static::$decodeTable)) {
-            static::$decodeTable = array_flip(static::$encodeTable);
-        }
-    }
+    protected static $decodeTable = [
+        'a' =>  0, 'b' =>  1, 'c' =>  2, 'd' =>  3, 'e' =>  4, 'f' => 5,
+        'g' =>  6, 'h' =>  7, 'i' =>  8, 'j' =>  9, 'k' => 10, 'l' => 11,
+        'm' => 12, 'n' => 13, 'o' => 14, 'p' => 15, 'q' => 16, 'r' => 17,
+        's' => 18, 't' => 19, 'u' => 20, 'v' => 21, 'w' => 22, 'x' => 23,
+        'y' => 24, 'z' => 25, 0   => 26, 1   => 27, 2   => 28, 3   => 29,
+        4   => 30, 5   => 31, 6   => 32, 7   => 33, 8   => 34, 9   => 35,
+    ];
 
     /**
      * Encode a hostname label to its Punycode version
@@ -68,171 +68,6 @@ trait Punycode
         }
 
         return static::encodeString($codePoints, $input);
-    }
-
-    /**
-     * Encode a string into its punycode version
-     *
-     * @param  array  $codePoints input code points
-     * @param  string $input      input string
-     *
-     * @return string
-     */
-    protected static function encodeString(array $codePoints, $input)
-    {
-        static::initTable();
-        $n      = static::INITIAL_N;
-        $bias   = static::INITIAL_BIAS;
-        $delta  = 0;
-        $h      = count($codePoints['basic']);
-        $b      = $h;
-        $i      = 0;
-        $length = mb_strlen($input, 'UTF-8');
-        $output = implode('', array_map([get_called_class(), 'codePointToChar'], $codePoints['basic']));
-        if ($b > 0) {
-            $output .= static::DELIMITER;
-        }
-
-        while ($h < $length) {
-            $m     = $codePoints['nonBasic'][$i++];
-            $delta = $delta + ($m - $n) * ($h + 1);
-            $n     = $m;
-            foreach ($codePoints['all'] as $c) {
-                if ($c < $n || $c < static::INITIAL_N) {
-                    $delta++;
-                }
-                if ($c === $n) {
-                    $q = $delta;
-                    for ($k = static::BASE;; $k += static::BASE) {
-                        $t = static::calculateThreshold($k, $bias);
-                        if ($q < $t) {
-                            break;
-                        }
-                        $code    = $t + (($q - $t) % (static::BASE - $t));
-                        $output .= static::$encodeTable[$code];
-                        $q       = ($q - $t) / (static::BASE - $t);
-                    }
-
-                    $output .= static::$encodeTable[$q];
-                    $bias    = static::adapt($delta, $h + 1, ($h === $b));
-                    $delta   = 0;
-                    $h++;
-                }
-            }
-            $delta++;
-            $n++;
-        }
-
-        return static::PREFIX.$output;
-    }
-
-    /**
-     * Is a submitted label a valid punycoded label
-     *
-     * @param  string  $input
-     *
-     * @return boolean  return true if a valid label is submitted
-     */
-    public static function decodeLabel($input)
-    {
-        $decoded = static::decodeString($input);
-        if (static::encodeLabel($decoded) == $input) {
-            return $decoded;
-        }
-
-        return $input;
-    }
-
-    /**
-     * Decode a punycode encoded hostname label
-     *
-     * @param string $input the punycode encoded label
-     *
-     * @return string Unicode hostname
-     */
-    protected static function decodeString($input)
-    {
-        if (strpos($input, static::PREFIX) !== 0) {
-            return $input;
-        }
-        $input  = substr($input, strlen(static::PREFIX));
-        $output = '';
-        $pos    = strrpos($input, static::DELIMITER);
-        if ($pos !== false) {
-            $output = substr($input, 0, $pos++);
-        }
-        $outputLength = strlen($output);
-        $inputLength  = strlen($input);
-        static::initTable();
-        $n    = static::INITIAL_N;
-        $i    = 0;
-        $bias = static::INITIAL_BIAS;
-        $pos  = (int) $pos;
-        while ($pos < $inputLength) {
-            for ($oldi = $i, $w = 1, $k = static::BASE;; $k += static::BASE) {
-                $digit = static::$decodeTable[$input[$pos++]];
-                $i     = $i + ($digit * $w);
-                $t     = static::calculateThreshold($k, $bias);
-                if ($digit < $t) {
-                    break;
-                }
-                $w = $w * (static::BASE - $t);
-            }
-
-            $bias = static::adapt($i - $oldi, ++$outputLength, ($oldi === 0));
-            $n    = $n + (int) ($i / $outputLength);
-            $i    = $i % ($outputLength);
-            $output = mb_substr($output, 0, $i, 'UTF-8')
-                .static::codePointToChar($n)
-                .mb_substr($output, $i, $outputLength - 1, 'UTF-8');
-            $i++;
-        }
-
-        return $output;
-    }
-
-    /**
-     * Calculate the bias threshold to fall between TMIN and TMAX
-     *
-     * @param int $k
-     * @param int $bias
-     *
-     * @return int
-     */
-    protected static function calculateThreshold($k, $bias)
-    {
-        if ($k <= $bias + static::TMIN) {
-            return static::TMIN;
-        }
-
-        if ($k >= $bias + static::TMAX) {
-            return static::TMAX;
-        }
-
-        return $k - $bias;
-    }
-
-    /**
-     * Bias adaptation
-     *
-     * @param int     $delta
-     * @param int     $numPoints
-     * @param boolean $firstTime
-     *
-     * @return int
-     */
-    protected static function adapt($delta, $numPoints, $firstTime)
-    {
-        $offset = 0;
-        $delta  = $firstTime ? floor($delta / static::DAMP) : $delta >> 1;
-        $delta += floor($delta / $numPoints);
-
-        $tmp = static::BASE - static::TMIN;
-        for (; $delta > $tmp * static::TMAX >> 1; $offset += static::BASE) {
-            $delta = floor($delta / $tmp);
-        }
-
-        return floor($offset + ($tmp + 1) * $delta / ($delta + static::SKEW));
     }
 
     /**
@@ -287,6 +122,60 @@ trait Punycode
     }
 
     /**
+     * Encode a string into its punycode version
+     *
+     * @param  array  $codePoints input code points
+     * @param  string $input      input string
+     *
+     * @return string
+     */
+    protected static function encodeString(array $codePoints, $input)
+    {
+        $n      = static::INITIAL_N;
+        $bias   = static::INITIAL_BIAS;
+        $delta  = 0;
+        $h      = count($codePoints['basic']);
+        $b      = $h;
+        $i      = 0;
+        $length = mb_strlen($input, 'UTF-8');
+        $output = implode('', array_map([get_called_class(), 'codePointToChar'], $codePoints['basic']));
+        if ($b > 0) {
+            $output .= static::DELIMITER;
+        }
+
+        while ($h < $length) {
+            $m     = $codePoints['nonBasic'][$i++];
+            $delta = $delta + ($m - $n) * ($h + 1);
+            $n     = $m;
+            foreach ($codePoints['all'] as $c) {
+                if ($c < $n || $c < static::INITIAL_N) {
+                    $delta++;
+                }
+                if ($c === $n) {
+                    $q = $delta;
+                    for ($k = static::BASE;; $k += static::BASE) {
+                        $t = static::calculateThreshold($k, $bias);
+                        if ($q < $t) {
+                            break;
+                        }
+                        $code    = $t + (($q - $t) % (static::BASE - $t));
+                        $output .= static::$encodeTable[$code];
+                        $q       = ($q - $t) / (static::BASE - $t);
+                    }
+                    $output .= static::$encodeTable[$q];
+                    $bias    = static::adapt($delta, $h + 1, ($h === $b));
+                    $delta   = 0;
+                    $h++;
+                }
+            }
+            $delta++;
+            $n++;
+        }
+
+        return static::PREFIX.$output;
+    }
+
+    /**
      * Convert a code point to its single or multi-byte character
      *
      * @param int $code
@@ -309,5 +198,112 @@ trait Punycode
 
         return chr(($code >> 18) + 240).chr((($code >> 12) & 63) + 128)
             .chr((($code >> 6) & 63) + 128).chr(($code & 63) + 128);
+    }
+
+    /**
+     * Calculate the bias threshold to fall between TMIN and TMAX
+     *
+     * @param int $k
+     * @param int $bias
+     *
+     * @return int
+     */
+    protected static function calculateThreshold($k, $bias)
+    {
+        if ($k <= $bias + static::TMIN) {
+            return static::TMIN;
+        }
+
+        if ($k >= $bias + static::TMAX) {
+            return static::TMAX;
+        }
+
+        return $k - $bias;
+    }
+
+    /**
+     * Bias adaptation
+     *
+     * @param int     $delta
+     * @param int     $numPoints
+     * @param boolean $firstTime
+     *
+     * @return int
+     */
+    protected static function adapt($delta, $numPoints, $firstTime)
+    {
+        $offset = 0;
+        $delta  = $firstTime ? floor($delta / static::DAMP) : $delta >> 1;
+        $delta += floor($delta / $numPoints);
+
+        $tmp = static::BASE - static::TMIN;
+        for (; $delta > $tmp * static::TMAX >> 1; $offset += static::BASE) {
+            $delta = floor($delta / $tmp);
+        }
+
+        return floor($offset + ($tmp + 1) * $delta / ($delta + static::SKEW));
+    }
+
+    /**
+     * Is a submitted label a valid punycoded label
+     *
+     * @param  string  $input
+     *
+     * @return boolean  return true if a valid label is submitted
+     */
+    public static function decodeLabel($input)
+    {
+        $decoded = static::decodeString($input);
+        if (static::encodeLabel($decoded) == $input) {
+            return $decoded;
+        }
+
+        return $input;
+    }
+
+    /**
+     * Decode a punycode encoded hostname label
+     *
+     * @param string $input the punycode encoded label
+     *
+     * @return string Unicode hostname
+     */
+    protected static function decodeString($input)
+    {
+        if (strpos($input, static::PREFIX) !== 0) {
+            return $input;
+        }
+        $input  = substr($input, strlen(static::PREFIX));
+        $output = '';
+        $pos    = strrpos($input, static::DELIMITER);
+        if ($pos !== false) {
+            $output = substr($input, 0, $pos++);
+        }
+        $outputLength = strlen($output);
+        $inputLength  = strlen($input);
+        $n    = static::INITIAL_N;
+        $i    = 0;
+        $bias = static::INITIAL_BIAS;
+        $pos  = (int) $pos;
+        while ($pos < $inputLength) {
+            for ($oldi = $i, $w = 1, $k = static::BASE;; $k += static::BASE) {
+                $digit = static::$decodeTable[$input[$pos++]];
+                $i     = $i + ($digit * $w);
+                $t     = static::calculateThreshold($k, $bias);
+                if ($digit < $t) {
+                    break;
+                }
+                $w = $w * (static::BASE - $t);
+            }
+            $bias = static::adapt($i - $oldi, ++$outputLength, ($oldi === 0));
+            $n    = $n + (int) ($i / $outputLength);
+            $i    = $i % ($outputLength);
+            $output = mb_substr($output, 0, $i, 'UTF-8')
+                .static::codePointToChar($n)
+                .mb_substr($output, $i, $outputLength - 1, 'UTF-8');
+            $i++;
+        }
+
+        return $output;
     }
 }

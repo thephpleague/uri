19c8a12c5605632fb58918de3f1c18eeb3317b77
nyamsprod <nyamsprod@gmail.com>
2016-06-09 10:45:19 +0200

    Adding a Relativize URI Modifier

--- test/Modifiers/UriModifierTest.php
+++ test/Modifiers/UriModifierTest.php
@@ -4,6 +4,7 @@ namespace League\Uri\Test\Modifiers;
 
 use InvalidArgumentException;
 use League\Uri\Modifiers\Normalize;
+use League\Uri\Modifiers\Relativize;
 use League\Uri\Modifiers\Resolve;
 use League\Uri\Schemes\Data as DataUri;
 use League\Uri\Schemes\Ftp as FtpUri;
@@ -16,6 +17,8 @@ use PHPUnit_Framework_TestCase as TestCase;
  */
 class UriModifierTest extends TestCase
 {
+    const BASE_URI = 'http://a/b/c/d;p?q';
+
     /**
      * @dataProvider resolveProvider
      * @param $uri
@@ -33,52 +36,50 @@ class UriModifierTest extends TestCase
 
     public function resolveProvider()
     {
-        $base_uri = 'http://a/b/c/d;p?q';
-
         return [
-            'base uri' =>                [$base_uri, '',               $base_uri],
-            'scheme' =>                  [$base_uri, 'http://d/e/f',   'http://d/e/f'],
-            'path 1' =>                  [$base_uri, 'g',              'http://a/b/c/g'],
-            'path 2' =>                  [$base_uri, './g',            'http://a/b/c/g'],
-            'path 3' =>                  [$base_uri, 'g/',             'http://a/b/c/g/'],
-            'path 4' =>                  [$base_uri, '/g',             'http://a/g'],
-            'authority' =>               [$base_uri, '//g',            'http://g'],
-            'query' =>                   [$base_uri, '?y',             'http://a/b/c/d;p?y'],
-            'path + query' =>            [$base_uri, 'g?y',            'http://a/b/c/g?y'],
-            'fragment' =>                [$base_uri, '#s',             'http://a/b/c/d;p?q#s'],
-            'path + fragment' =>         [$base_uri, 'g#s',            'http://a/b/c/g#s'],
-            'path + query + fragment' => [$base_uri, 'g?y#s',          'http://a/b/c/g?y#s'],
-            'single dot 1' =>            [$base_uri, '.',              'http://a/b/c/'],
-            'single dot 2' =>            [$base_uri, './',             'http://a/b/c/'],
-            'single dot 3' =>            [$base_uri, './g/.',          'http://a/b/c/g/'],
-            'single dot 4' =>            [$base_uri, 'g/./h',          'http://a/b/c/g/h'],
-            'double dot 1' =>            [$base_uri, '..',             'http://a/b/'],
-            'double dot 2' =>            [$base_uri, '../',            'http://a/b/'],
-            'double dot 3' =>            [$base_uri, '../g',           'http://a/b/g'],
-            'double dot 4' =>            [$base_uri, '../..',          'http://a/'],
-            'double dot 5' =>            [$base_uri, '../../',         'http://a/'],
-            'double dot 6' =>            [$base_uri, '../../g',        'http://a/g'],
-            'double dot 7' =>            [$base_uri, '../../../g',     'http://a/g'],
-            'double dot 8' =>            [$base_uri, '../../../../g',  'http://a/g'],
-            'double dot 9' =>            [$base_uri, 'g/../h' ,        'http://a/b/c/h'],
-            'mulitple slashes' =>        [$base_uri, 'foo////g',       'http://a/b/c/foo////g'],
-            'complex path 1' =>          [$base_uri, ';x',             'http://a/b/c/;x'],
-            'complex path 2' =>          [$base_uri, 'g;x',            'http://a/b/c/g;x'],
-            'complex path 3' =>          [$base_uri, 'g;x?y#s',        'http://a/b/c/g;x?y#s'],
-            'complex path 4' =>          [$base_uri, 'g;x=1/./y',      'http://a/b/c/g;x=1/y'],
-            'complex path 5' =>          [$base_uri, 'g;x=1/../y',     'http://a/b/c/y'],
-            'dot segments presence 1' => [$base_uri, '/./g', 'http://a/g'],
-            'dot segments presence 2' => [$base_uri, '/../g', 'http://a/g'],
-            'dot segments presence 3' => [$base_uri, 'g.', 'http://a/b/c/g.'],
-            'dot segments presence 4' => [$base_uri, '.g', 'http://a/b/c/.g'],
-            'dot segments presence 5' => [$base_uri, 'g..', 'http://a/b/c/g..'],
-            'dot segments presence 6' => [$base_uri, '..g', 'http://a/b/c/..g'],
-            'origin uri without path' => ['http://h:b@a', 'b/../y',    'http://h:b@a/y'],
-            '2 relative paths 1'      => ['a/b',          '../..',     '/'],
-            '2 relative paths 2'      => ['a/b',          './.',       'a/'],
-            '2 relative paths 3'      => ['a/b',          '../c',      'c'],
-            '2 relative paths 4'      => ['a/b',          'c/..',      'a/'],
-            '2 relative paths 5'      => ['a/b',          'c/.',       'a/c/'],
+            'base uri' =>                [self::BASE_URI, '',              self::BASE_URI],
+            'scheme' =>                  [self::BASE_URI, 'http://d/e/f',  'http://d/e/f'],
+            'path 1' =>                  [self::BASE_URI, 'g',             'http://a/b/c/g'],
+            'path 2' =>                  [self::BASE_URI, './g',           'http://a/b/c/g'],
+            'path 3' =>                  [self::BASE_URI, 'g/',            'http://a/b/c/g/'],
+            'path 4' =>                  [self::BASE_URI, '/g',            'http://a/g'],
+            'authority' =>               [self::BASE_URI, '//g',           'http://g'],
+            'query' =>                   [self::BASE_URI, '?y',            'http://a/b/c/d;p?y'],
+            'path + query' =>            [self::BASE_URI, 'g?y',           'http://a/b/c/g?y'],
+            'fragment' =>                [self::BASE_URI, '#s',            'http://a/b/c/d;p?q#s'],
+            'path + fragment' =>         [self::BASE_URI, 'g#s',           'http://a/b/c/g#s'],
+            'path + query + fragment' => [self::BASE_URI, 'g?y#s',         'http://a/b/c/g?y#s'],
+            'single dot 1' =>            [self::BASE_URI, '.',             'http://a/b/c/'],
+            'single dot 2' =>            [self::BASE_URI, './',            'http://a/b/c/'],
+            'single dot 3' =>            [self::BASE_URI, './g/.',         'http://a/b/c/g/'],
+            'single dot 4' =>            [self::BASE_URI, 'g/./h',         'http://a/b/c/g/h'],
+            'double dot 1' =>            [self::BASE_URI, '..',            'http://a/b/'],
+            'double dot 2' =>            [self::BASE_URI, '../',           'http://a/b/'],
+            'double dot 3' =>            [self::BASE_URI, '../g',          'http://a/b/g'],
+            'double dot 4' =>            [self::BASE_URI, '../..',         'http://a/'],
+            'double dot 5' =>            [self::BASE_URI, '../../',        'http://a/'],
+            'double dot 6' =>            [self::BASE_URI, '../../g',       'http://a/g'],
+            'double dot 7' =>            [self::BASE_URI, '../../../g',    'http://a/g'],
+            'double dot 8' =>            [self::BASE_URI, '../../../../g', 'http://a/g'],
+            'double dot 9' =>            [self::BASE_URI, 'g/../h' ,       'http://a/b/c/h'],
+            'mulitple slashes' =>        [self::BASE_URI, 'foo////g',      'http://a/b/c/foo////g'],
+            'complex path 1' =>          [self::BASE_URI, ';x',            'http://a/b/c/;x'],
+            'complex path 2' =>          [self::BASE_URI, 'g;x',           'http://a/b/c/g;x'],
+            'complex path 3' =>          [self::BASE_URI, 'g;x?y#s',       'http://a/b/c/g;x?y#s'],
+            'complex path 4' =>          [self::BASE_URI, 'g;x=1/./y',     'http://a/b/c/g;x=1/y'],
+            'complex path 5' =>          [self::BASE_URI, 'g;x=1/../y',    'http://a/b/c/y'],
+            'dot segments presence 1' => [self::BASE_URI, '/./g',          'http://a/g'],
+            'dot segments presence 2' => [self::BASE_URI, '/../g',         'http://a/g'],
+            'dot segments presence 3' => [self::BASE_URI, 'g.',            'http://a/b/c/g.'],
+            'dot segments presence 4' => [self::BASE_URI, '.g',            'http://a/b/c/.g'],
+            'dot segments presence 5' => [self::BASE_URI, 'g..',           'http://a/b/c/g..'],
+            'dot segments presence 6' => [self::BASE_URI, '..g',           'http://a/b/c/..g'],
+            'origin uri without path' => ['http://h:b@a', 'b/../y',        'http://h:b@a/y'],
+            '2 relative paths 1'      => ['a/b',          '../..',         '/'],
+            '2 relative paths 2'      => ['a/b',          './.',           'a/'],
+            '2 relative paths 3'      => ['a/b',          '../c',          'c'],
+            '2 relative paths 4'      => ['a/b',          'c/..',          'a/'],
+            '2 relative paths 5'      => ['a/b',          'c/.',           'a/c/'],
         ];
     }
 
@@ -99,6 +100,92 @@ class UriModifierTest extends TestCase
         $ftp = FtpUri::createFromString('ftp//a/b/c/d;p');
         $modifier = new Resolve($http);
         $modifier->__invoke($ftp);
+    }
+
+    /**
+     * @dataProvider relativizeProvider
+     */
+    public function testRelativize($uri, $resolved, $expected)
+    {
+        $uri      = HttpUri::createFromString($uri);
+        $resolved = HttpUri::createFromString($resolved);
+        $modifier = new Relativize($uri);
+
+        $this->assertSame($expected, (string) $modifier->__invoke($resolved));
+    }
+
+    public function relativizeProvider()
+    {
+        return [
+            'different scheme'        => [self::BASE_URI,       'https://a/b/c/d;p?q',   'https://a/b/c/d;p?q'],
+            'different authority'     => [self::BASE_URI,       'https://g/b/c/d;p?q',   'https://g/b/c/d;p?q'],
+            'empty uri'               => [self::BASE_URI,       '',                      ''],
+            'same uri'                => [self::BASE_URI,       self::BASE_URI,          'd;p?q'],
+            'same path'               => [self::BASE_URI,       'http://a/b/c/d;p',      'd;p'],
+            'parent path 1'           => [self::BASE_URI,       'http://a/b/c/',         './'],
+            'parent path 2'           => [self::BASE_URI,       'http://a/b/',           '../'],
+            'parent path 3'           => [self::BASE_URI,       'http://a/',             '../../'],
+            'parent path 4'           => [self::BASE_URI,       'http://a',              '../../'],
+            'sibling path 1'          => [self::BASE_URI,       'http://a/b/c/g',        'g'],
+            'sibling path 2'          => [self::BASE_URI,       'http://a/b/c/g/h',      'g/h'],
+            'sibling path 3'          => [self::BASE_URI,       'http://a/b/g',          '../g'],
+            'sibling path 4'          => [self::BASE_URI,       'http://a/g',            '../../g'],
+            'query'                   => [self::BASE_URI,       'http://a/b/c/d;p?y',    'd;p?y'],
+            'fragment'                => [self::BASE_URI,       'http://a/b/c/d;p?q#s',  'd;p?q#s'],
+            'path + query'            => [self::BASE_URI,       'http://a/b/c/g?y',      'g?y'],
+            'path + fragment'         => [self::BASE_URI,       'http://a/b/c/g#s',      'g#s'],
+            'path + query + fragment' => [self::BASE_URI,       'http://a/b/c/g?y#s',    'g?y#s'],
+            'mulitple slashes'        => [self::BASE_URI,       'http://a/b/c/foo////g', 'foo////g'],
+            'mulitple slashes 1'      => [self::BASE_URI,       'http://a/b////c/foo/g', '..////c/foo/g'],
+            'relative single dot 1'   => [self::BASE_URI,       '.',                     '.'],
+            'relative single dot 2'   => [self::BASE_URI,       './',                    './'],
+            'relative double dot 1'   => [self::BASE_URI,       '..',                    '..'],
+            'relative double dot 2'   => [self::BASE_URI,       '../',                   '../'],
+            'path with colon 1'       => ['http://a/b/c/d:p?q', 'http://a/b/c/d:p',      './d:p'],
+            'path with colon 2'       => [self::BASE_URI,       'http://a/b/c/g/d:p',    'g/d:p'],
+        ];
+    }
+
+    /**
+     * @dataProvider emptyAbsolutePathProvider
+     */
+    public function testResolvedAndRelativizedAddEmptyAbsolutePath(
+        $baseUri,
+        $uri,
+        $expectedRelativize,
+        $expectedResolved
+    ) {
+        $baseUri = HttpUri::createFromString($baseUri);
+        $resolver = new Resolve($baseUri);
+        $relativizer = new Relativize($baseUri);
+        $uri = HttpUri::createFromString($uri);
+
+        $relativeUri = $relativizer($uri);
+        $resolvedUri = $resolver($relativeUri);
+
+        $this->assertSame($expectedRelativize, (string) $relativeUri);
+        $this->assertSame($expectedResolved, (string) $resolvedUri);
+    }
+
+    public function emptyAbsolutePathProvider()
+    {
+        return [
+            'empty path'            => [self::BASE_URI, 'http://a/', '../../', 'http://a/'],
+            'absolute empty path'   => [self::BASE_URI, 'http://a',  '../../', 'http://a/'],
+            'relative single dot 1' => [self::BASE_URI, '.',         '.',      'http://a/b/c/'],
+            'relative single dot 2' => [self::BASE_URI, './',        './',     'http://a/b/c/'],
+            'relative double dot 1' => [self::BASE_URI, '..',        '..',     'http://a/b/'],
+            'relative double dot 2' => [self::BASE_URI, '../',       '../',    'http://a/b/'],
+        ];
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function relativizeLetThrowUriException()
+    {
+        $dataUri = DataUri::createFromString('data:text/plain;charset=us-ascii,Bonjour%20le%20monde!');
+        (new Relativize($dataUri1))->__invoke($dataUri);
     }
 
     /**

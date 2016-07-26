11a693669d0d3a706b4f87cd3bfc402012d2c45d
nyamsprod <nyamsprod@gmail.com>
2016-07-26 15:27:47 +0200

    Improve Normalize Modifier

--- src/Modifiers/Normalize.php
+++ src/Modifiers/Normalize.php
@@ -12,6 +12,7 @@
 namespace League\Uri\Modifiers;
 
 use League\Uri\Interfaces\Uri;
+use League\Uri\Types\TranscoderTrait;
 use Psr\Http\Message\UriInterface;
 
 /**
@@ -23,6 +24,8 @@ use Psr\Http\Message\UriInterface;
  */
 class Normalize extends AbstractUriModifier
 {
+    use TranscoderTrait;
+
     /**
      * Return a Uri object modified according to the modifier
      *
@@ -32,16 +35,50 @@ class Normalize extends AbstractUriModifier
      */
     public function __invoke($uri)
     {
-        $modifier = new Pipeline([
-            new HostToAscii(),
-            new KsortQuery(),
-        ]);
+        $modifier = new Pipeline([new HostToAscii(), new KsortQuery()]);
 
         $path = $uri->getPath();
         if ('' !== $uri->getScheme().$uri->getAuthority() || (isset($path[0]) && '/' === $path[0])) {
             $modifier = $modifier->pipe(new RemoveDotSegments());
         }
 
-        return $modifier($uri);
+        return $this->decodeUri($modifier($uri));
+    }
+
+    /**
+     * Decode specific component of the URI
+     *
+     * @param Uri|UriInterface $uri
+     *
+     * @return Uri|UriInterface
+     */
+    protected function decodeUri($uri)
+    {
+        foreach (['Path', 'Query', 'Fragment'] as $part) {
+            $uri = $this->decodeUriPart($uri, $part);
+        }
+
+        return $uri;
+    }
+
+    /**
+     * Decode an URI part
+     *
+     * @param Uri|UriInterface $uri
+     * @param string           $property
+     *
+     * @return Uri|UriInterface
+     */
+    protected function decodeUriPart($uri, $property)
+    {
+        $value = preg_replace_callback(
+            ',%('.self::$unreservedCharsEncoded.'),i',
+            function (array $matches) {
+                return rawurldecode($matches[0]);
+            },
+            call_user_func([$uri, 'get'.$property])
+        );
+
+        return call_user_func([$uri, 'with'.$property], $value);
     }
 }

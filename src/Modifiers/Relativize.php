d7f082d3532d8c648427ce2c486d4db4ae604278
nyamsprod <nyamsprod@gmail.com>
2016-11-24 12:59:59 +0100

    prepare 4.2.1 release

--- src/Modifiers/Relativize.php
+++ src/Modifiers/Relativize.php
@@ -6,7 +6,7 @@
  * @author    Ignace Nyamagana Butera <nyamsprod@gmail.com>
  * @copyright 2016 Ignace Nyamagana Butera
  * @license   https://github.com/thephpleague/uri/blob/master/LICENSE (MIT License)
- * @version   4.2.0
+ * @version   4.1.0
  * @link      https://github.com/thephpleague/uri/
  */
 namespace League\Uri\Modifiers;

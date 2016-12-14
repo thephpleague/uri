2daaa898a9952e7d33421909889c3f4c6130a8c6
nyamsprod <nyamsprod@gmail.com>
2016-12-14 09:41:20 +0100

    the library is now a metapackge

--- src/functions_include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Don't redefine the functions if included multiple times.
-if (!function_exists('League\Uri\Modifiers\uri_reference')) {
-    require __DIR__.'/Modifiers/functions.php';
-}

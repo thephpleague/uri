04457bf5aa139c5ccd6b3acb6f88cba4feaff08a
Kamil Kokot <kamil@kokot.me>
2017-10-11 10:28:06 +0200

    Make MIME type tests accepting both "text/x-vcard" and "text/vcard" for VCards

--- src/Components/DataPath.php
+++ src/Components/DataPath.php
@@ -132,10 +132,6 @@ class DataPath extends AbstractComponent implements DataPathInterface
             throw new InvalidArgumentException(sprintf('invalid mimeType, `%s`', $mimetype));
         }
 
-        if ($mimetype === 'text/vcard') {
-            $mimetype = 'text/x-vcard';
-        }
-
         return $mimetype;
     }
 

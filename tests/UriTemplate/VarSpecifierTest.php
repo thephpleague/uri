50bd0617e2991ba90ae15d2d09b5eafa3399b80e
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2020-05-15 23:11:09 +0200

    Splitting responsability in URI template generation to remove array usage

---
+++ tests/UriTemplate/VarSpecifierTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * League.Uri (https://uri.thephpleague.com)
+ *
+ * (c) Ignace Nyamagana Butera <nyamsprod@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace LeagueTest\Uri\UriTemplate;
+
+use League\Uri\Exceptions\SyntaxError;
+use League\Uri\UriTemplate\VarSpecifier;
+use PHPUnit\Framework\TestCase;
+use function var_export;
+
+/**
+ * @coversDefaultClass \League\Uri\UriTemplate\VarSpecifier
+ */
+final class VarSpecifierTest extends TestCase
+{
+    /**
+     * @covers ::fromString
+     * @covers ::toString
+     * @covers ::__construct
+     *
+     * @dataProvider providesValidNotation
+     */
+    public function testItCanBeInstantiatedWithAValidNotation(string $notation): void
+    {
+        self::assertSame($notation, VarSpecifier::fromString($notation)->toString());
+    }
+
+    public function providesValidNotation(): iterable
+    {
+        return [
+            'simple' => ['notation' => 'var'],
+            'variable with underscore' => ['notation' => 'under_score'],
+            'variable with digit' => ['notation' => 'd1g1t'],
+            'variable with encoded' => ['notation' => 'semi%3Bcolon'],
+            'variable with point' => ['notation' => 'p.int'],
+            'with explode modifier' => ['notation' => 'var*'],
+            'with position modifier' => ['notation' => 'var:5'],
+            'complex variable' => ['notation' => 'und.er_sc0re%3B:5'],
+        ];
+    }
+
+    /**
+     * @covers ::fromString
+     *
+     * @dataProvider providesInvalidNotation
+     */
+    public function testItFailsToInstantiatedWithAnInvalidNotationString(string $notation): void
+    {
+        self::expectException(SyntaxError::class);
+
+        VarSpecifier::fromString($notation);
+    }
+
+    public function providesInvalidNotation(): iterable
+    {
+        return [
+            'using the explode modifier with the position notation' => ['notation' => 'var:*'],
+            'with a position modifier ouf of bound' => ['notation' => 'var:10000'],
+            'empty string' => ['notation' => ''],
+            'unsupported character' => ['notation' => 'bébé'],
+        ];
+    }
+
+    /**
+     * @covers ::position
+     * @covers ::modifier
+     * @covers ::name
+     */
+    public function testItCanReturnsTheVarSpecifierProperties(): void
+    {
+        $varSpecifier = VarSpecifier::fromString('und.er_sc0re%3B:5');
+
+        self::assertSame('und.er_sc0re%3B', $varSpecifier->name());
+        self::assertSame(':', $varSpecifier->modifier());
+        self::assertSame(5, $varSpecifier->position());
+
+        $varSpecifier = VarSpecifier::fromString('und.er_sc0re%3B*');
+
+        self::assertSame('und.er_sc0re%3B', $varSpecifier->name());
+        self::assertSame('*', $varSpecifier->modifier());
+        self::assertSame(0, $varSpecifier->position());
+    }
+
+    /**
+     * @covers ::__set_state
+     */
+    public function testSetState(): void
+    {
+        $varSpecifier = VarSpecifier::fromString('und.er_sc0re%3B*');
+
+        self::assertEquals($varSpecifier, eval('return '.var_export($varSpecifier, true).';'));
+    }
+}
---
+++ tests/UriTemplate/VariableBagTest.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * League.Uri (https://uri.thephpleague.com)
+ *
+ * (c) Ignace Nyamagana Butera <nyamsprod@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace LeagueTest\Uri\UriTemplate;
+
+use ArrayIterator;
+use League\Uri\Exceptions\TemplateCanNotBeExpanded;
+use League\Uri\UriTemplate\VariableBag;
+use PHPUnit\Framework\TestCase;
+use function var_export;
+
+/**
+ * @coversDefaultClass \League\Uri\UriTemplate\VariableBag
+ */
+final class VariableBagTest extends TestCase
+{
+    /**
+     * @covers ::assign
+     * @covers ::__construct
+     * @covers ::all
+     * @covers ::normalizeValue
+     *
+     * @param array<string, string|array<string>> $expected
+     *
+     * @dataProvider provideValidIterable
+     */
+    public function testItCanBeInstantiatedWithAnIterable(iterable $iterable, array $expected): void
+    {
+        $bag = new VariableBag($iterable);
+
+        self::assertEquals($expected, $bag->all());
+    }
+
+    public function provideValidIterable(): iterable
+    {
+        return [
+            'array' => [
+                'iterable' => ['name' => 'value'],
+                'expected' => ['name' => 'value'],
+            ],
+            'iterable' => [
+                'iterable' => new ArrayIterator(['name' => 'value']),
+                'expected' => ['name' => 'value'],
+            ],
+            'empty array' =>  [
+                'iterable' => [],
+                'expected' => [],
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::assign
+     * @covers ::normalizeValue
+     * @covers ::fetch
+     *
+     * @param mixed                $value    the value to be assign to the name
+     * @param string|array<string> $expected
+     *
+     * @dataProvider provideValidAssignParameters
+     */
+    public function testItCanAssignNameAndValuesToTheBag(string $name, $value, $expected): void
+    {
+        $bag = new VariableBag();
+        $bag->assign($name, $value);
+
+        self::assertSame($expected, $bag->fetch($name));
+    }
+
+    public function provideValidAssignParameters(): iterable
+    {
+        return [
+            'string' => [
+                'name' => 'foo',
+                'value' => 'bar',
+                'expected' => 'bar',
+            ],
+            'integer' => [
+                'name' => 'foo',
+                'value' => 12,
+                'expected' => '12',
+            ],
+            'bool' => [
+                'name' => 'foo',
+                'value' => false,
+                'expected' => '0',
+            ],
+            'list' => [
+                'name' => 'foo',
+                'value' => ['bar', true, 42],
+                'expected' => ['bar', '1', '42'],
+            ],
+            'empty string' => [
+                'name' => 'foo',
+                'value' => '',
+                'expected' => '',
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::assign
+     * @covers ::normalizeValue
+     * @covers ::__construct
+     */
+    public function testItWillFailToAssignUnsupportedType(): void
+    {
+        self::expectException(\TypeError::class);
+
+        new VariableBag(['name' => new \stdClass()]);
+    }
+
+    /**
+     * @covers ::assign
+     * @covers ::normalizeValue
+     * @covers ::__construct
+     */
+    public function testItWillFailToAssignNestedList(): void
+    {
+        self::expectException(TemplateCanNotBeExpanded::class);
+
+        new VariableBag(['name' => ['foo' => ['bar' => 'baz']]]);
+    }
+
+    /**
+     * @covers ::__set_state
+     */
+    public function testSetState(): void
+    {
+        $bag = new VariableBag(['foo' => 'bar', 'yolo' => 42, 'list' => [1, 2, 'three']]);
+
+        self::assertEquals($bag, eval('return '.var_export($bag, true).';'));
+    }
+
+    public function testItCanReplaceItsValueWithThatOfAnotherInstance(): void
+    {
+        $bag = new VariableBag([
+            'foo' => 'bar',
+            'list' => [1, 2, 'three'],
+        ]);
+
+        $defaultBag = new VariableBag([
+            'foo' => 'bar',
+            'yolo' => 42,
+            'list' => 'this is a list',
+        ]);
+
+        $expected = new VariableBag([
+            'foo' => 'bar',
+            'yolo' => 42,
+            'list' => [1, 2, 'three'],
+        ]);
+
+        self::assertEquals($expected, $bag->replace($defaultBag));
+        self::assertEquals($defaultBag, $defaultBag->replace($bag));
+    }
+}

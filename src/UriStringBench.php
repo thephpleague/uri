1f5f89787acad8ee88254dd9ae149b681545dce6
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2022-09-08 14:03:35 +0200

    Adding composer command

--- src/UriStringBench.php
+++ src/UriStringBench.php
@@ -19,18 +19,18 @@ final class UriStringBench
 {
     #[Bench\OutputTimeUnit('seconds')]
     #[Bench\Assert('mode(variant.mem.peak) < 2097152'), Bench\Assert('mode(variant.time.avg) < 10000000')]
-    public function testParsingARegularUri(): void
+    public function benchParsingARegularUri(): void
     {
         $uri = 'https://uri.thephpleague.com:1337/5.0?query=value1&query=value2#foobar';
 
-        for ($i = 0; $i < 1_000_000; $i++) {
+        for ($i = 0; $i < 100_000; $i++) {
             UriString::parse($uri);
         }
     }
 
     #[Bench\OutputTimeUnit('seconds')]
     #[Bench\Assert('mode(variant.mem.peak) < 2097152'), Bench\Assert('mode(variant.time.avg) < 10000000')]
-    public function testBuildingARegularUri(): void
+    public function benchBuildingARegularUri(): void
     {
         $components = [
             'scheme' => 'https',
@@ -43,7 +43,7 @@ final class UriStringBench
             'fragment' => 'foobar',
         ];
 
-        for ($i = 0; $i < 1_000_000; $i++) {
+        for ($i = 0; $i < 100_000; $i++) {
             UriString::build($components);
         }
     }
